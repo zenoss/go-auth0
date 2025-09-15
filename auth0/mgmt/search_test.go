383683c1774c3ca7e07a45026ee97643c27c4eab
Danylo Kondratiev <danylo@posteo.ie>
2025-09-15 16:36:50 +0300

    Fix mocks import

--- auth0/mgmt/search_test.go
+++ auth0/mgmt/search_test.go
@@ -9,8 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/zenoss/go-auth0/auth0/http"
+	mocks "github.com/zenoss/go-auth0/auth0/http/mocks"
 	"github.com/zenoss/go-auth0/auth0/mgmt"
-	"github.com/zenoss/go-auth0/mocks"
 )
 
 const testJson = `{
